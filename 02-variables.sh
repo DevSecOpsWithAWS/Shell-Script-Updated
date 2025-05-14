1dd61b953de9bb9ce117d43316f05bc3ff41749d
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-14 11:11:27 +0530

    02-variables

---
+++ 02-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Manvi: Hi Mommy!"
+echo "Mommy: Hi Manvi!"
+echo "Manvi: How are you?"
+echo "Mommy: I am fine, how are you?"
+echo "Manvi: I am also fine."
+echo "Mommy: What are you doing?"
+echo "Manvi: I am learning shell scripting."
+echo "Mommy: That's great! Keep it up."
+echo "Manvi: Thank you, Mommy!"
+echo "Mommy: You're welcome, Manvi!"
+echo "Manvi: Bye, Mommy!"
+
