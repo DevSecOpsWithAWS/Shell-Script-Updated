dc7a347c6dbe7e19283071804a88ccc192ff30da
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-14 11:13:55 +0530

    03-variables

---
+++ 03-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+PERSON1="Manvi"
+PERSON2="Mommy"
+echo "$PERSON1: Hi $PERSON2!"
+echo "$PERSON2: Hi $PERSON1!"
+echo "$PERSON1: How are you?"
+echo "$PERSON2: I am fine, how are you?"
+echo "$PERSON1: I am also fine."
+echo "$PERSON2: What are you doing?"
+echo "$PERSON1: I am learning shell scripting."
+echo "$PERSON2: That's great! Keep it up."
+echo "$PERSON1: Thank you, $PERSON2!"
+echo "$PERSON2: You're welcome, $PERSON1!"
+echo "$PERSON1: Bye, $PERSON2!"
