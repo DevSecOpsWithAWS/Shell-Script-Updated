8a4028c578b1fb26da1a876a0eddc684098a36da
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-20 07:39:18 +0530

    08-special-variables

---
+++ 08-special-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo ("$1 --> first argument")
+echo ("$2 --> second argument")
+echo ("$3 --> third argument")
+echo ("$0 --> Script Name")
+echo ("$@ --> All arguments passed")
+echo ("$# --> number of variables passed")
+echo ("$PWD --> present working directory")
+echo ("$HOME --> home directoy of current user")
+echo ("$USER --> which user running the script")
+echo ("$$ --> Process ID of current script")
+echo ("$? --> exit status of last command")
+sleep 60 &
+echo ("$! --> process id of last background command")
