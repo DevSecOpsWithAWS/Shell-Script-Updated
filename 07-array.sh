2b27a5d9bc753bc30145e2e7c7dd9982a8f27def
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-20 07:24:54 +0530

    07-arrays

---
+++ 07-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "krrish" "dangal" "3idiots" "tanhaji")
+
+echo "First movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[1]}"
+echo "Third movie is ${MOVIES[2]}"
+echo "Fourth movie is ${MOVIES[3]}"
+echo "Fifth movie is ${MOVIES[4]}"
+
+echo "All movies are ${#MOVIES[@]}"
