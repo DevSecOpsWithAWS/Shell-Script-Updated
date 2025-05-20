a77338817c8e420f8136457f29794ab077cd2de9
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-20 07:22:16 +0530

    06-data-types

--- 06-dataTypes.sh
+++ 06-dataTypes.sh
@@ -9,5 +9,6 @@ TIMESTAMP=$(date)
 echo "Script started executing at $TIMESTAMP"
 echo "The first number is $NUMBER1"
 echo "The second number is $NUMBER2"
-echo "SUM of $NUMBER1 and $NUMBER2 is $NUMBER1+$NUMBER2"
+SUM=$(($NUMBER1 + $NUMBER2))
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
 
