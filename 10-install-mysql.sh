7405a3d3169f0abd0798044d6ea08e6b54a09163
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-20 08:02:22 +0530

    10-mysql-install-script

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -10,8 +10,23 @@ then
   echo "Error:: You must have root privileges to run this script"
   exit 1
 else
-  echo "$?"
   echo "You have root privileges"
   dnf install mysql -y
 fi
 
+dnf list installed mysql
+echo "$?"
+
+if [ $? -ne 0 ]
+then
+  dnf install mysql -y
+  if [ $? -ne 0]
+  then
+    echo "installing MySQL...Failure"
+    exit 1
+  else
+    echo "MySQL installation completed successfully"
+  fi
+else
+  echo "MySQL is already installed"
+fi
