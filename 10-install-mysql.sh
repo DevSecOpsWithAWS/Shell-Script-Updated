9b0f5ec47334482bc589bd40aad6bf5d6cb6bca8
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-05-20 07:51:22 +0530

    10-mysql-install-script

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 
-dnf install mysql -y
 
+
+USERID=$(id -u)
+if [ $USERID -ne 0]
+  echo "Error:: You must have root privileges to run this script"
+  exit 1
+fi
+
+dnf install mysql -y
